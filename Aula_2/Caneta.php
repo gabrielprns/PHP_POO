814f6cef8eee96293d7841464912f0ee7e1f3e8d
gabrielprns <gabrielpedrornn@outlook.com>
2022-07-12 11:25:31 -0300

    Project

---
+++ Aula_2/Caneta.php
@@ -0,0 +1,20 @@
+<?php
+class caneta{
+
+  var $modelo;
+  var $cor;
+  var $ponta;
+  var $carga;
+  var $tampada;
+   function rabiscar(){
+    
+   }
+   function tampar(){
+
+   }
+   function destampar(){
+
+   }
+}
+
+?>
